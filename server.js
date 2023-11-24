1f825cbb656b386c1d378a1db2c8bb1c1bf539f1
Layla Hammoud <laylahammoud63@gmail.com>
2023-11-24 16:33:59 +0200

    add morgan

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import userRoute from './routes/user.js'
 import transactionRoute from './routes/transactions.js'
 import db from './models/index.js'
+import morgan from "morgan";
 // Create an instance of Express
 const app = express();
 
@@ -11,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use(morgan('dev'))
 
 app.use("/api/users", userRoute);
 app.use("/api/transactions", transactionRoute);
