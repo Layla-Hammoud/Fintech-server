56766e82b907837885f47b8b40dae8ba057d67c5
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-11-29 15:24:49 +0200

    adding authentication and authorisation to the transaction routes

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import { isAuthenticated, isAuthorizedUser} from '../middlware/authMiddleware.js';
 import { createTransaction,deleteTransaction,editTransaction, getTransactions } from '../controllers/transaction.js'
 
 const router=express.Router();
 
-router.post('/',createTransaction);
-router.put('/edit-transaction',editTransaction)
-router.delete('/delete-transaction',deleteTransaction)
-router.get('/',getTransactions)
+router.post('/',isAuthenticated,isAuthorizedUser(['user','merchant']),createTransaction);
+router.put('/edit-transaction',isAuthenticated,isAuthorizedUser(['merchant']),editTransaction)
+router.delete('/delete-transaction',isAuthenticated,isAuthorizedUser(['user','merchant','admin']),deleteTransaction)
+router.get('/',isAuthenticated,isAuthorizedUser(['user','merchant','admin']),getTransactions)
 export default router;
